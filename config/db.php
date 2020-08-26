b40c296230d81d05a23bde79581f725ddb1659cb
Emmanuel97418 <70200889+Emmanuel97418@users.noreply.github.com>
2020-08-26 10:11:52 +0400

    Update db.php

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=127.0.0.1;dbname=yii2basic',
-    'username' => 'root',
-    'password' => 'root',
+    'username' => 'administrateur',
+    'password' => 'administrateur',
     'charset' => 'utf8',
 
     # Schema cache options (for production environment)
