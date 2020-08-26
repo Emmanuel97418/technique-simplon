ccd96ef059a9f060a0ea0423f9c098c1cb26484a
Debese Emmanuel <emmanuel.debese15@icloud.com>
2020-08-26 09:54:06 +0400

    update db.php

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=db',
-    'username' => 'administrateur',
-    'password' => 'administrateur',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'root',
+    'password' => 'root',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
