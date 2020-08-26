a9bf380754843f147bcd5af410e81e35247777f2
Debese Emmanuel <emmanuel.debese15@icloud.com>
2020-08-26 09:12:12 +0400

    update db.php

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=myqsl://technique_s_4947:G0xH8FzzDe-3KrWIIEwp@a3648f76-849f-47bf-8eb1-576aca6d9b7a.technique-s-4947.mysql.dbs.scalingo.com:31491/technique_s_4947;dbname=yii2basic',
-    'username' => 'root',
-    'password' => 'root',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'username',
+    'password' => 'password',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
